2b367ebf857dad1fe2a385a32bcfdbb08ee8022a
Evgeniy Kharitonov <47530159+SilAres@users.noreply.github.com>
2022-08-31 08:36:08 +0300

    Update main.py

--- project/main.py
+++ project/main.py
@@ -12,7 +12,7 @@ cam_new = "192.168.1.108"
 network_cam["network.eth0.IPAddress"] = cam_new
 network_cam["Network.Hostname"] = name
 
-# dahua_video(cam, headers, video_date)
-# dahua_ntp(cam, headers, ntp_cam)
-# dahua_name(cam, headers, name)
-# dahua_network(cam, headers, network_cam)
+dahua_video(cam, headers, video_date)
+dahua_ntp(cam, headers, ntp_cam)
+dahua_name(cam, headers, name)
+dahua_network(cam, headers, network_cam)
